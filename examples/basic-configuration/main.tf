ddf127e58d51f60e9a5b04ec447a285297d84b4c
Akshai2022 <105642067+Akshai2022@users.noreply.github.com>
2023-01-06 11:06:49 +0530

    Update main.tf

--- examples/basic-configuration/main.tf
+++ examples/basic-configuration/main.tf
@@ -1,4 +1,4 @@
-module "tagsmap" {
+/* module "tagsmap" {
   source                = "../../../tagsmap" #"git::https://gitlab.wuintranet.net/terraform-module-registry/aws/tagsmap?ref=v1.0.0"
   application_name      = "Foundation Services"
   application_code      = "FDS"
@@ -14,13 +14,13 @@ module "tagsmap" {
   request_id            = "LZ"
   pci                   = "No"
   support_contact       = "ioc_ccoe@wu.com"
-}
+} */
 
 
 module "sns" {
   source              = "../.." #"git::https://gitlab.wuintranet.net/terraform-module-registry/aws/sns?ref=v1.0.1"
-  naming_prefix_upper = module.tagsmap.naming_prefix_upper
-  common_tags         = module.tagsmap.common_tags
+  naming_prefix_upper = "TEST" #module.tagsmap.naming_prefix_upper
+  #common_tags         = #module.tagsmap.common_tags
   function            = "Automation"
   increment_code      = "004"
   description         = "Sample SNS created by automation pillar"
